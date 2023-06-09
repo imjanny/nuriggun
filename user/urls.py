fe739b6ce28e162bbc357edf179c88ab1ae47f94
imjanny <wogns0808@gmail.com>
2023-06-09 20:18:15 +0900

    :sparkles: FEAT : kakao , naver , google login create
    social account 기능구현

--- user/urls.py
+++ user/urls.py
@@ -13,13 +13,6 @@ urlpatterns = [
     #토큰
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    #소셜로그인
-    path('kakao/login/', views.kakao_login, name='kakao_login'),
-    path('kakao/callback/', views.kakao_callback, name='kakao_callback'),
-    path('kakao/login/finish/', views.KakaoLogin.as_view(), name='kakao_login_todjango'),
-    path('github/login/', views.github_login, name='github_login'),
-    path('github/callback/', views.github_callback, name='github_callback'),
-    path('github/login/finish/', views.GithubLogin.as_view(), name='github_login_todjango'),
 
     # 비밀번호 재설정 URL
     path('password/reset/', PasswordResetView.as_view(), name='rest_password_reset'),
