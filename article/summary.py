fc73b67ae63a25324a2431101b491df02648719d
Kim Kyungjin <kyungjin2215@gmail.com>
2023-06-30 16:36:57 +0900

    🩹Fix: 기사 요약 기능 자잘한 수정 #102

--- article/summary.py
+++ article/summary.py
@@ -1,8 +1,20 @@
+import threading
+
 import openai
 import os
 from dotenv import load_dotenv
 
 from article.models import Article
+
+
+class SummaryThread(threading.Thread):
+    '''비동기전송 : 게시글 작성 시 요약 기능으로 인한 지연현상이 없어짐'''
+    def __init__(self, content):
+        self.content = content
+        threading.Thread.__init__(self)
+
+    def run(self):
+        summary(self.content)
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -15,13 +27,14 @@ def summary(content):
             summary_content = queryset.order_by("-id").first()
         response = openai.Completion.create(
             engine='text-davinci-003',
-            prompt="Please summarize this news article in Korean, focusing only on the key points. Exclude any unnecessary information such as greetings or reporter information. The summary should be around 500 tokens. \n\n[News Article]"+content,
+            prompt="Summarize the content in Korean, but keep it simple so that it does not exceed max_tokens=500. No greetings or reporter information is required."+content,
             max_tokens=500,
             temperature=0.3,
             n=1,
             stop=None,
         )
-        summary = response.choices[0].text.strip()
-        summary_content.summary = summary
+
+        answer = response.choices[0].text.strip()
+
+        summary_content.summary = answer
         summary_content.save()
-        return summary
