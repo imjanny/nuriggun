d725da3fd31de19b0bc496f657f4993ecdca2ede
hyunah <hil02165@naver.com>
2023-07-07 02:28:38 +0900

    :white_check_mark: Feat: user) update SignUpViewTest #18

--- user/tests.py
+++ user/tests.py
@@ -10,92 +10,59 @@ from django.utils.encoding import force_bytes
 
 # 회원가입 TEST
 class SignUpViewTest(APITestCase):
+    def setUp(self):
+        '''회원가입 : 이메일 중복 확인을 위한 유저 생성'''
+        self.user = User.objects.create_user(
+            email='testgo@test.test', password='G1843514dadg23@')
+        
     def test_signup_1(self):
-        '''비밀번호 유효성 검사'''
+        '''회원가입 : 비밀번호 유효성 검사'''
         url = reverse("sign_up_view")
         data = {
             "email": "test@test.test",
             "password": "123456"
         }
         response = self.client.post(url, data)
-        # print(response.data)
         self.assertEqual(response.status_code, 400)
 
     def test_signup_2(self):
-        '''비밀번호 빈값'''
+        '''회원가입 : 이메일/비밀번호 빈값'''
         url = reverse("sign_up_view")
         data = {
-            "email": "test@test.test"
         }
         response = self.client.post(url, data)
-        # print(response.data)
         self.assertEqual(response.status_code, 400)
 
     def test_signup_3(self):
-        '''이메일 빈값'''
-        url = reverse("sign_up_view")
-        data = {
-            "password": "123456"
-        }
-        response = self.client.post(url, data)
-        # print(response.data)
-        self.assertEqual(response.status_code, 400)
-
-    def test_signup_4(self):
-        '''이메일/비밀번호 빈값'''
-        url = reverse("sign_up_view")
-        data = {
-        }
-        response = self.client.post(url, data)
-        # print(response.data)
-        self.assertEqual(response.status_code, 400)
-
-    def setUp(self):
-        '''이메일 중복 확인을 위한 유저 생성'''
-        self.user = User.objects.create_user(
-            email='testgo@test.test', password='G1843514dadg23@')
-
-    def test_signup_5(self):
-        '''이메일 중복'''
+        '''회원가입 : 이메일 중복'''
         url = reverse("sign_up_view")
         data = {
             "email": "testgo@test.test",
             "password": "123456"
         }
         response = self.client.post(url, data)
-        # print(response.data)
         self.assertEqual(response.status_code, 400)
 
-    def test_signup_6(self):
-        '''회원가입 성공!'''
+    def test_signup_4(self):
+        '''회원가입 : 성공! (+이메일 전송 확인)'''
         url = reverse("sign_up_view")
         data = {
             "email": "test@test.test",
             "password": "abc123qw!"
         }
         response = self.client.post(url, data)
-        # print(response.data)
-        self.assertEqual(response.status_code, 201)
-
-    # 이메일 실제 전송x, 백엔드 이메일
-    @override_settings(EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend")
-    def test_signup_email_send_7(self):
-        '''이메일 전송 확인'''
-        url = reverse("sign_up_view")
-        data = {
-            "email": "test@test.test",
-            "password": "abc123qw!"
-        }
-        response = self.client.post(url, data)
-        # print(response.data)
         self.assertEqual(response.status_code, 201)
 
         # 이메일 비동기 전송으로 시간 연장 추가
-        async def wait_for_email():
-            while len(mail.outbox) < 1:
+        async def wait_for_email(max_seconds=5):
+            elapsed_seconds = 0
+            while len(mail.outbox) < 1 and elapsed_seconds < max_seconds:
                 await asyncio.sleep(0.1)
+                elapsed_seconds += 0.1
+            if len(mail.outbox) < 1:
+                raise TimeoutError("이메일을 기다리는 동안 시간 초과가 발생했습니다.")
 
-        asyncio.run(wait_for_email())
+        asyncio.run(wait_for_email(max_seconds=5))
 
         self.assertEqual(len(mail.outbox), 1)
 
